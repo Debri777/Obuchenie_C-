21c5add2f1aba73a4b285d1c2f830d94f18e5cc2
Evgeniy <Evgeniy@DESKTOP-BEH56V1>
2023-04-19 21:50:11 +0300

    Добавьте файлы проекта.

---
+++ Obuchenie_C#/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+Console.Clear();
+Console.WriteLine("Enter a number: ");
+string number = Console.ReadLine();
+int i = number.Length;
+
+if (i == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Corresponds to-Palindrome");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Does not match-Palindrome");
+    }
+}
+else
+{
+    Console.WriteLine($"{number} - not a five-digit number: ");
+}
